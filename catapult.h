7cf0f449c03e8d46a33a1251d589d23b26249989
vedant8 <vedant8basu@gmail.com>
2015-10-31 17:18:57 +0530

    Create catapult.h

---
+++ catapult.h
@@ -0,0 +1,46 @@
+#include<simplecpp>
+#include<conio.h>
+#include<cstdio>
+/*
+#include"tracker.cpp"
+#define*/
+class Catapult//:public tracker
+{public:
+    double cx,cy,x1,y1,f;
+    Circle C1;
+    Rectangle *b;
+    Rectangle *l1;
+
+void fire(bool pl)// write code using bl!!
+    {
+ }//end of fire()
+
+ void swing(bool pl){
+    
+ }
+
+void moveC(bool pl)
+{
+}
+void create(bool pl)
+    {
+    }
+
+    void canRotate()
+       {
+      
+       }
+    void aim(bool pl)
+        {
+        
+        }
+
+double getY1(){
+    }
+
+double getX1(){    
+}
+
+};
+
+
