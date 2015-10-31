2d0ca85d695c5cd9706d21929e8bd733710f848d
vedant8 <vedant8basu@gmail.com>
2015-10-31 17:26:30 +0530

    Create Block.h

---
+++ Block.h
@@ -0,0 +1,19 @@
+
+#include<simplecpp>
+
+class Block{
+double cx,cy;
+    int health;
+public:
+Rectangle *b;
+
+
+    Block()
+    Block(double c1,double c2)
+     void create(double c1,double c2,bool pl)
+     void explode()
+    int damage(int d)
+double GX()
+double GY()
+
+};
